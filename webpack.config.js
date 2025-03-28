d98ba43cc3c550109f2bf4dfcb39df7351aa0e36
Luca Morgado <luca.morgado.pro@gmail.com>
2025-03-28 11:03:05 +0100

    added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+const path = require('path');
+
+module.exports = async function (env, argv) {
+  const config = await createExpoWebpackConfigAsync(env, argv);
+  
+  // Add polyfills for node core modules
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    "crypto": require.resolve("crypto-browserify"),
+    "stream": require.resolve("stream-browserify"),
+    "buffer": require.resolve("buffer/"),
+    "util": require.resolve("util/"),
+    "vm": require.resolve("vm-browserify")  // Add vm polyfill
+  };
+
+  // Add plugins for the polyfills
+  const webpack = require('webpack');
+  config.plugins.push(
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer'],
+    })
+  );
+
+  return config;
+};
